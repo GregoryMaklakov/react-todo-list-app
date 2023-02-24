d6b823826787ad14f0227ea53f28cfd36e363379
Gregory <maklakov.grigory@gmail.com>
2023-02-24 17:21:19 +0100

    add .npmrc

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import styles from './Tags.module.css';
 
-export const Tag = () => {
-    return (
+// export const Tag = () => {
+//     return (
 
-    )
-}
+//     )
+// }

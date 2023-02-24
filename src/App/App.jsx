1cc73814655a9db9117383683864eaf52cceb115
Gregory <maklakov.grigory@gmail.com>
2023-02-24 08:58:28 +0100

    add Checkbox component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,18 @@
 import { Input } from "../components/Input/Input";
-//import styles from "./App.module.css";
+import styles from "./App.module.css";
 import { useState } from 'react';
+import { Checkbox } from "../components/Checkbox/Checkbox";
 
 
-function App() {
+function App({ }) {
   const [inputValue, setInputValue] = useState('');
+  const [check, setCheck] = useState(false);
 
   return (
-    <div>
+    <div className={styles.container}>
       <Input value={inputValue} onChange={setInputValue} />
+      <br /><br />
+      <Checkbox checked={check} onChange={setCheck}>Done</Checkbox>
     </div>
   )
 }

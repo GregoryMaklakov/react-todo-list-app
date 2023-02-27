2c41bda19d306ad032a36f54fea86a88c4c8047f
Gregory <maklakov.grigory@gmail.com>
2023-02-27 15:49:37 +0100

    add variant for checkbox

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -15,7 +15,7 @@ function App({ }) {
       <br />
 
       <div className={styles.buttonsAll}>
-        <Checkbox checked={check} onChange={setCheck}>
+        <Checkbox checked={check} onChange={setCheck} icon='IconCheckbox' variant='icon'>
           Done
         </Checkbox>
         <Button className={styles.buttonPrimary} variant="primary">

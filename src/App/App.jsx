ab35a461f2017a6bc7b3f834dc3efa3ae8f7a9a5
Gregory <maklakov.grigory@gmail.com>
2023-03-07 12:49:11 +0100

    fin lesson 8.7, add tags(bug with actions)

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -11,11 +11,57 @@ import {
 } from "../components";
 
 function App() {
+  //сохранять todo
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Todo 1",
+      text: "Text 1",
+      done: false,
+      tags: [1, 2],
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      text: "Text 2",
+      done: false,
+      tags: [1, 3],
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      text: "Text 3",
+      done: false,
+      tags: [3],
+    },
+  ]);
+  //сохранять todo id(edit)
+  const [editTodoId, setEditTodoId] = useState(null);
+  //Удалять todo id
+  const [deleteTodoId, setDeleteTodoId] = useState(null);
+  //Tags
+  const [tags, setTags] = useState([
+    {
+      id: 1,
+      color: "blue",
+      name: "work",
+    },
+    {
+      id: 2,
+      color: "red",
+      name: "study",
+    },
+    {
+      id: 3,
+      color: "purple",
+      name: "family",
+    },
+  ]);
+  const [activeTagId, setActideTagId] = useState(null);
+  // ======
   const [inputValue, setInputValue] = useState("");
   const [check, setCheck] = useState(false);
-
   const [done, onDoneChange] = useState(false);
-
   const [onDelete, setOnDelete] = useState(false);
   const [onDeleted, setOnDeleted] = useState(true);
 
@@ -29,6 +75,42 @@ function App() {
 
   return (
     <div className={styles.container}>
+      <div>
+        {tags.map((tag) => {
+          return (
+            <Tag
+              key={tag.id}
+              color={tag.color}
+              active={activeTagId === tag.id}
+              isEditable
+              onClick={() => setActideTagId(tag.id)}
+              onSave={() => {
+                const copy = [...tags];
+                const idx = copy.findIndex(({ id }) => id === tag.id);
+                //hasMatch проверяем на наличии такого же value в теге
+                const hasMatch = tags.some(({ name }) => name === value);
+                if (idx >= 0 && !hasMatch) {
+                  copy.splice(idx, 1, { ...tag, name: value });
+                  setTags(copy);
+                  return true;
+                }
+                return false;
+              }}
+              onDelete={() => {
+                const copy = [...tags];
+                const idx = copy.findIndex(({ id }) => id === tag.id);
+
+                if (idx >= 0) {
+                  copy.splice(idx, 1);
+                  setTags(copy);
+                }
+              }}
+            >
+              {tag.name}
+            </Tag>
+          );
+        })}
+      </div>
       <Input value={inputValue} onChange={setInputValue} />
       <div className={styles.buttonsAll}>
         <Checkbox checked={check} onChange={setCheck}>
@@ -111,7 +193,10 @@ function App() {
           onDelete={() => setOnDelete(!onDelete)}
           onEdit={() => undefined}
           onDoneChange={onDoneChange}
-          tags={[]}
+          tags={[
+            { color: "red", id: 1, name: "red" },
+            { color: "blue", id: 2, name: "blue" },
+          ]}
         ></TodoCard>
       )}
       <Tag

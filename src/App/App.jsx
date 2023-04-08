e6c01468e32edfe6ae696a35c858e081be63686d
Gregory <maklakov.grigory@gmail.com>
2023-04-08 18:26:53 +0200

    add animation

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -44,7 +44,7 @@ function App() {
   const animatedTag = {
     hidden: { opacity: 0, x: "-100%" },
     show: { opacity: 1, x: 0, transition: { staggerChildren: 0.2 } },
-    exit: { opacity: 0, x: "-100%", transition: { duration: 0.5 } },
+    exit: { opacity: 0, x: "-100%", transition: { duration: 0.4 } },
   };
 
   // ?========================= ANIMATION ======================================
